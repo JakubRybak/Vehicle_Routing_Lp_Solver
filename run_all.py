5a9f49f795e7237c1e8848df2ebf050e698fd94d
41570 <01189048@pw.edu.pl>
2026-06-14 13:31:18 +0200

    optimized ilp

--- run_all.py
+++ run_all.py
@@ -1,19 +1,18 @@
 import os
-from cvrp_ilp import solve_cvrp_ilp
+from cvrp_ilp_opt import solve_cvrp_ilp
 
 def run_experiments():
     instances = [
+        os.path.join('data', 'A', 'A-n7-k3.vrp'),
         os.path.join('data', 'E', 'E-n22-k4.vrp'),
-        os.path.join('data', 'E', 'E-n23-k3.vrp'),
-        os.path.join('data', 'E', 'E-n30-k3.vrp'),
-        os.path.join('data', 'E', 'E-n33-k4.vrp')
+        os.path.join('data', 'E', 'E-n23-k3.vrp')
     ]
     
     for inst in instances:
         if os.path.exists(inst):
-            print(f"--- Uruchamianie dla: {inst} ---")
+            print(f"--- Uruchamianie zoptymalizowanego ILP dla: {inst} ---")
             try:
-                solve_cvrp_ilp(inst)
+                solve_cvrp_ilp(inst, time_limit=600)
             except Exception as e:
                 print(f"Błąd podczas analizy {inst}: {e}")
         else:
